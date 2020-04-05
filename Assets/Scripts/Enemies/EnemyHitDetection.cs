b2193442f7ae2a2d2b8a68773dcc23787d130ef9
Pedro <Oliveira>
2020-04-05 17:08:23 +0100

    added enemyhitDetection script

---
+++ Assets/Scripts/Enemies/EnemyHitDetection.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHitDetection : MonoBehaviour
+{
+    enum destroy
+    {
+        me,
+        collidedObj,
+        parentOfCollidedObj,
+        referenceObject
+    }
+
+    [SerializeField]
+    private string selectedTag;
+
+    [SerializeField]
+    private destroy destroyCollidedObject;
+    [SerializeField]
+    private GameObject objectoReference;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(selectedTag))
+        {
+            switch (destroyCollidedObject)
+            {
+                case destroy.me:
+                    {
+                        Destroy(this.gameObject);
+                        break;
+                    }
+                case destroy.referenceObject:
+                    {
+                        Destroy(objectoReference);
+                        break;
+                    }
+
+                case destroy.collidedObj:
+                    {
+                        Destroy(other.gameObject);
+                        break;
+                    }
+
+                case destroy.parentOfCollidedObj:
+                    {
+                        Destroy(other.transform.parent.gameObject);
+                        break;
+                    }
+            }
+        }
+    }
+}
