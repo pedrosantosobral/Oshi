2bb0b3cb81aa3b58c184ac383aae40cb3483ddad
Pedro Santos <pedrosantosobral@gmail.com>
2020-04-05 17:45:52 +0100

    Add tuturial scene

---
+++ Assets/Scripts/Level/LevelGenerator_Tutorial.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using CustomEventSystem;
+public class LevelGenerator_Tutorial : MonoBehaviour
+{
+
+    //LoadingScreen variables
+    [SerializeField] private VoidEvent onPlayerSpawn;
+    public Animator loadingScreen;
+
+
+    private bool    _calculationDone;
+    public  bool    readyToPlayer;
+
+    public LayerMask Room;
+
+    //variable to stop the map generation
+    public bool        stopGeneration;
+
+    //variables to control the room spawning timer
+    private float       _timeBetweenRooms;
+    [SerializeField]
+    private float       _startTimeBetweenRooms = 0.25f;
+    private void Start()
+    {
+        //TimerBetweenRoomSpawns();
+        stopGeneration = true;
+        CreateAIGraph();
+        _calculationDone = true;
+        
+        
+
+    }
+   
+    private void CreateAIGraph()
+    {
+        if(stopGeneration == true)
+        {
+            if (_calculationDone == false)
+            {
+                Invoke("ScanGraph", 0.1f);
+            }
+            
+        }
+    }
+
+    private void ScanGraph()
+    {
+        AstarPath.active.Scan();
+        Invoke("ReadyToPlayer", 0.1f);
+    }
+
+    private void ReadyToPlayer()
+    {
+        readyToPlayer = true;
+        loadingScreen.SetBool("FadeScreen", readyToPlayer);
+        onPlayerSpawn.Raise();
+    }
+
+}
---
+++ Assets/Scripts/Level/SpawnPlayerAfterLvlGeneration_Tuturial.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlayerAfterLvlGeneration_Tutorial : MonoBehaviour
+{
+
+    public  GameObject player;
+    private bool       _doOnce;
+    private LevelGenerator_Tutorial _lvlGenReference;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _doOnce = false;
+        _lvlGenReference = GameObject.Find("LevelGenerator_Tutorial").GetComponent<LevelGenerator_Tutorial>();  
+    }
+
+    private void Update()
+    {
+        if (_lvlGenReference.readyToPlayer == true && _doOnce == false)
+        {
+            Instantiate(player, gameObject.transform.position, Quaternion.identity);
+            _doOnce = true;
+        }
+    }
+
+
+
+
+}
