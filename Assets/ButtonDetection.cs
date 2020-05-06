f57296a0196104d1517bb9730d432dc15501327b
Pedro Santos <pedrosantosobral@gmail.com>
2020-05-06 21:38:11 +0100

    add initial teleport UI

---
+++ Assets/ButtonDetection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonDetection : MonoBehaviour
+{
+    private LevelGenerator _lvlGeneratorReference;
+
+    public GameObject buttonToActivate;
+
+    public int line;
+    public int column;
+
+    public Pos buttonPos;
+    private void Start()
+    {
+        buttonPos.line = line;
+        buttonPos.column = column;
+        _lvlGeneratorReference = GameObject.Find("LevelGenerator").GetComponent<LevelGenerator>();
+    }
+
+    public void CheckButtonActivity()
+    {
+        if (!_lvlGeneratorReference.teleportRoomPositions.Contains(buttonPos))
+        {
+            buttonToActivate.SetActive(false);
+        }
+    }
+
+}
