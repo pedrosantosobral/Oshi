e6a422e671c01a268bd5089ab4edf8a303450de3
Pedro Santos <pedrosantosobral@gmail.com>
2020-05-17 18:52:25 +0100

    Animate open Text

---
+++ Assets/OshiTEXTAnimation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OshiTEXTAnimation : MonoBehaviour
+{
+    [SerializeField] private LeanTweenType textAnimation;
+    [SerializeField] private float animationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeanTween.scale(gameObject, new Vector3(3f, 1.5f, 0), animationSpeed).setEase(textAnimation);   
+    }
+}
