9f228db8f82ed6dda743460c378d695cbc7f28d0
Pedro Santos <pedrosantosobral@gmail.com>
2020-06-04 16:51:25 +0100

    Add TutorialTips Prefab

---
+++ Assets/TutorialTips.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTips : MonoBehaviour
+{
+    [SerializeField] private GameObject text;
+    [SerializeField] private LeanTweenType textIN;
+    [SerializeField] private LeanTweenType textOUT;
+    [SerializeField] private float animSpeed;
+
+    private void Start()
+    {
+        LeanTween.scale(text, new Vector3(0, 0, 0), 0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        LeanTween.scale(text, new Vector3(1, 1, 0), animSpeed).setEase(textIN);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        LeanTween.scale(text, new Vector3(0, 0, 0), animSpeed).setEase(textOUT);
+    }
+
+
+}
