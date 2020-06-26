3b377b40269ea5745c298a9dcdb0e3cc9bbcad8d
Pedro Santos <pedrosantosobral@gmail.com>
2020-06-26 23:38:50 +0100

    Add Win music

--- Assets/Scripts/EndLevel.cs
+++ Assets/Scripts/EndLevel.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class EndLevel : MonoBehaviour
 {
+
+    [SerializeField] private AudioClip winMusic;
+
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            AudioManager.Instance.PlayMusicWithCrossFade(winMusic);
             SceneManager.LoadScene("WinScreen");
         }
       
